9d6ab836332888ead66cc2f415c80628d9034cc6
Anurag Singh <principalquantum30@gmail.com>
2025-04-09 01:40:52 +0530

    codeforces #2093A : maths

---
+++ Codeforces-Solutions/A/2093A-Ideal_Generator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>         
+#include <queue>
+#include <stack>
+#include <algorithm>
+using namespace std;
+#define ll long long
+#define ld long double
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%2==0)cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    
+    
+    return 0;
+    
+}
